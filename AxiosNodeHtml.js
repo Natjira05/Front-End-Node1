66959453fd69dfe0b2e44bd587500a1475bdbf8f
Natjira05 <s6406021621099@email.kmutnb.ac.th>
2023-09-19 19:15:09 +0700

    test6

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -44,7 +44,7 @@ app.get("/create", (req, res) => {
 
 app.post("/create", async (req, res) => {
   try {
-    const data = {title: req.body.title, author: req.body.author};
+    const data = { title: req.body.title, author: req.body.author};
     await axios.post(base_url + '/books', data);
     res.redirect("/");
   } catch (err) {
